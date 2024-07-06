ef42e4405a479c50965b754ab3837094d2c20ccb
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-07-06 19:21:05 +0530

    Min and Max in Array.cpp

---
+++ array/Min and Max in Array.cpp
@@ -0,0 +1,44 @@
+//Min and Max in Array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    pair<long long, long long> getMinMax(vector<long long int> arr) {
+        long long mini = LLONG_MAX;
+        long long maxi = LLONG_MIN;
+        for(int i = 0;i<arr.size();i++){
+            mini = min(mini,arr[i]);
+            maxi = max(maxi,arr[i]);
+        }
+        return make_pair(mini,maxi);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<long long int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        pair<long long, long long> pp = ob.getMinMax(arr);
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
