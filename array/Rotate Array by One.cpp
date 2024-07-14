eaadb67b14e7197930b096e99227166e8a22a754
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-07-14 10:56:37 +0530

    Rotate Array by One.cpp

---
+++ array/Rotate Array by One.cpp
@@ -0,0 +1,47 @@
+//Rotate Array by One
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        for(int i = arr.size()-1;i>0;i--){
+            swap(arr[i],arr[i-1]);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        ob.rotate(arr);
+        for (long long i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
