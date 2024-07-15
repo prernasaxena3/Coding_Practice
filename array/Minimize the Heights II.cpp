ca90581d458dda325f32bea71973b7f8539395b8
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-07-15 19:30:40 +0530

    Minimize the Heights II.cpp

---
+++ array/Minimize the Heights II.cpp
@@ -0,0 +1,57 @@
+//Minimize the Heights II
+
+/*Given an array arr[] denoting heights of N towers and a positive integer K.
+For each tower, you must perform exactly one of the following operations exactly once.
+Increase the height of the tower by K
+Decrease the height of the tower by K
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+You can find a slight modification of the problem here.
+Note: It is compulsory to increase or decrease the height by K for each tower. After the operation, the resultant array should not contain any negative integers.*/
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        sort(arr,arr+n);
+        int ans = arr[n-1]-arr[0];
+        
+        int smallest = arr[0]+k;
+        int largest = arr[n-1]-k;
+        int small,large;
+        for(int i = 0;i<n-1;i++){
+            small = min(smallest,arr[i+1]-k);
+            large = max(largest,arr[i]+k);
+            if(small<0) continue;
+            ans = min(ans,large-small);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getMinDiff(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
