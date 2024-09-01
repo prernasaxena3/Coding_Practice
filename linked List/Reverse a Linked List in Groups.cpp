28bfa7f6b7e22c48a4bb8298bd19948c313dcada
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-09-02 00:21:52 +0530

    Create Reverse a Linked List in Groups.cpp

---
+++ linked List/Reverse a Linked List in Groups.cpp
@@ -0,0 +1,138 @@
+//Reverse a Linked List in Groups
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+   // Redirecting input from file
+  freopen("/Users/debojyoti.mandal/expt/input.txt", "r", stdin);
+
+  // Redirecting output to file
+  freopen("/Users/debojyoti.mandal/expt/output.txt", "w", stdout);
+*/
+
+struct node {
+    int data;
+    struct node* next;
+
+    node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to print linked list */
+void printList(struct node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element
+  Return the node which points to the head of the new LinkedList
+  Node is defined as
+    struct node
+    {
+        int data;
+        struct node* next;
+
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+
+    }*head;
+*/
+
+class Solution {
+  public:
+  
+    node* solve(struct node *head, int size, int k){
+        if(head == NULL){
+            return head;
+        }
+        int count = 0;
+        node*prev= NULL;
+        node* curr = head;
+        node* next = NULL;
+        while(curr != NULL && count<k){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        if(next != NULL){
+            head->next = solve(next,size-k,k);
+        }
+        
+        return prev;
+    }
+    
+    struct node *reverseIt(struct node *head, int k) {
+        int size = 0;
+        node* temp = head;
+        while(temp != NULL){
+            temp = temp->next;
+            size++;
+        }
+        return solve(head,size,k);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void) {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        node* head = new node(data);
+        node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new node(data);
+            tail = tail->next;
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        Solution ob;
+        head = ob.reverseIt(head, k);
+        printList(head);
+    }
+
+    return (0);
+}
+
+// } Driver Code Ends
