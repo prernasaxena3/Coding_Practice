3fed5aae36aa30b8ca048c26e9915626ba1c4690
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-09-02 23:40:10 +0530

    Create Kth from End of Linked List.cpp

---
+++ linked List/Kth from End of Linked List.cpp
@@ -0,0 +1,103 @@
+//Kth from End of Linked List
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+// Function to find the data of kth node from the end of a linked list.
+class Solution {
+  public:
+  
+    int getKthFromLast(Node *head, int k) {
+    
+        Node* temp = head;
+        Node* prev = head;
+        int size = 0;
+        while(temp){
+            temp = temp->next;
+            size++;
+        }
+        if(size<k){
+            return -1;
+        }
+        temp = head;
+        for(int i = 1;i<=size-k;i++){
+            prev = prev->next;
+        }
+        return prev->data;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        cout << ob.getKthFromLast(head, x) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
