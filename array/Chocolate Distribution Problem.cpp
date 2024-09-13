0e0918a900892033cce0e42b75fddf9cb61bd825
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-09-13 20:18:37 +0530

    Create Chocolate Distribution Problem.cpp

---
+++ array/Chocolate Distribution Problem.cpp
@@ -0,0 +1,52 @@
+//Chocolate Distribution Problem
+/*Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. 
+Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        sort(a.begin(),a.end());
+        long long int mini = INT_MAX;
+        int i = 0;
+        int j = m-1;
+        while(j<n){
+            long long int diff = a[j] - a[i];
+            mini = min(mini,diff);
+            i++;
+            j++;
+        }
+        return mini;
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
