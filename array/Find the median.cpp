df1ee88f8cc2d413babfd5a40a858ef44d75fe69
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-09-21 21:08:20 +0530

    Create Find the median.cpp

---
+++ array/Find the median.cpp
@@ -0,0 +1,45 @@
+//Find the median
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		    sort(v.begin(),v.end());
+		    int n = v.size();
+		    int ans = 0;
+		    if(n%2 != 0){
+		        ans = v[n/2];
+		    }else{
+		        int sum = v[n/2]+v[(n/2)-1];
+		        ans = sum/2;
+		    }
+		    return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
